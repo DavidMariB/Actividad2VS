a7f27501a3591e9d67d70e3bf393b507f39a4b90
David Marí Beltrán <david--_97@hotmail.com>
2018-01-11 18:39:25 +0100

    pipo

---
+++ Actividad2VS/Form1.Designer.vb
@@ -0,0 +1,128 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(214, 104)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(517, 73)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Obtener un numero entre 1 y 100"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(207, 230)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(284, 37)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Teclea un numero:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(513, 230)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(150, 38)
+        Me.TextBox1.TabIndex = 2
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 31
+        Me.ListBox1.Location = New System.Drawing.Point(214, 358)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(517, 190)
+        Me.ListBox1.TabIndex = 3
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(245, 615)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(156, 58)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Limpiar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(511, 615)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(152, 58)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Salir"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(207, 307)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(313, 37)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Numeros Tecleados:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(965, 716)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Actividad2VS/Form1.vb
@@ -0,0 +1,76 @@
+﻿Public Class Form1
+
+    Dim nRandom As Integer
+    Dim contador As Integer = 0
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Randomize()
+        nRandom = Int(100 * Rnd() + 1)
+        'MessageBox.Show("El numero aleatorio es: " + nRandom.ToString, "Numero Random")
+
+        Button1.Enabled = False
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As KeyEventArgs) Handles TextBox1.KeyDown
+
+        If e.KeyCode = Keys.Enter Then
+            Try
+                If Button1.Enabled Then
+                    MessageBox.Show("Debes generar un numero aletorio antes de poder adivinarlo", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    TextBox1.Text = ""
+                ElseIf Integer.Parse(TextBox1.Text) < 0 Then
+                    MessageBox.Show("No puedes introducir un numero negativo", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    TextBox1.Text = ""
+                ElseIf TextBox1.Text.Equals("") Then
+                    MessageBox.Show("Tienes que introducir un numero", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    TextBox1.Text = ""
+                ElseIf Integer.Parse(TextBox1.Text) < 1 Or Integer.Parse(TextBox1.Text) > 100 Then
+                    MessageBox.Show("Debes introducir un numero entre 1 y 100", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    TextBox1.Text = ""
+                ElseIf Integer.Parse(TextBox1.Text) < nRandom Then
+                    ListBox1.Items.Add(TextBox1.Text + " El numero secreto es mayor")
+                    ListBox1.TopIndex = ListBox1.Items.Count - 1
+                    TextBox1.Text = ""
+                    contador = contador + 1
+                ElseIf Integer.Parse(TextBox1.Text) > nRandom Then
+                    ListBox1.Items.Add(TextBox1.Text + " El numero secreto es menor")
+                    ListBox1.TopIndex = ListBox1.Items.Count - 1
+                    TextBox1.Text = ""
+                    contador = contador + 1
+                ElseIf Integer.Parse(TextBox1.Text) = nRandom Then
+                    TextBox1.Text = ""
+                    contador = contador + 1
+                    Select Case MessageBox.Show("Has acertado el numero secreto. ¿Quieres volver a jugar? Intentos utilizados: " + contador.ToString, "¡Enhorabuena!", MessageBoxButtons.YesNo, MessageBoxIcon.Information)
+                        Case DialogResult.Yes
+                            funcionLimpiar()
+                        Case DialogResult.No
+                            funcionSalir()
+                    End Select
+                End If
+            Catch ex As FormatException
+                MessageBox.Show("Solo se pueden introducir numeros enteros y positivos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                TextBox1.Text = ""
+            End Try
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        funcionLimpiar()
+    End Sub
+
+    Sub funcionLimpiar()
+        Button1.Enabled = True
+        ListBox1.Items.Clear()
+        TextBox1.Text = ""
+        contador = 0
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        funcionSalir()
+    End Sub
+
+    Sub funcionSalir()
+        Application.Exit()
+    End Sub
+End Class
